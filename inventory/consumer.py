320fcb590389bbc472f53a74477457a9f9aac65b
tuzun89 <to.uzunoglu@gmail.com>
2022-04-30 22:35:07 +0100

    redis stream for purchase

--- inventory/consumer.py
+++ inventory/consumer.py
@@ -13,7 +13,14 @@ while True:
     try:
         results = redis.xreadgroup(group, key, {key: '>'}, None)
 
-        print(results)
+        if results != []:
+            for result in results:
+                obj = result[1][0][1]
+                product = Product.get(obj['product_id'])
+                print(product)
+                product.quantity = product.quantity - int(obj['quantity'])
+                product.save()
+                
     except Exception as e:
         print(str(e))
     time.sleep(1)
